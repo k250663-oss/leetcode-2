c39f868241d2bac55a68dff477d243560e29de67
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-11-21 22:52:13 +0500

    Update move zeros.c

--- move zeros.c
+++ move zeros.c
@@ -13,12 +13,13 @@ void movezero(){
 for(int i=0 ; i<num ; i++){
 printf("%d " , arr[i]);
 }
-for(int i=0 ; i<num-1 ; i++){
-    if(arr[i]>arr[i+1]){
-        temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
-    }
+for(int i = 0; i < num- 1; i++){
+    for(int j = 0; j< num -1 - i;j++){
+        if(arr[j] >arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1] = temp;}
+            }
 }
 int j=0;
 for(int i=0;i<num;i++){
